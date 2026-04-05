71c21a803727a36350bbfd7f8920c0b4dc8571b0
Piyush Prajapat <piyushprajapati099@gmail.com>
2026-04-05 23:44:48 +0530

    Delete assessment  directory

--- assessment /finance_backend/services/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Service layer grouped by domain so the flow is easy to review."""
-
-from .dashboard import DashboardService
-from .records import RecordService
-from .users import UserService
-
-__all__ = ["DashboardService", "RecordService", "UserService"]
